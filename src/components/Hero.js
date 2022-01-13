96c71ff26a2d9b10aaf1d984d6a26c21c56e0bae
Ronald Marques <ronaldpmarques01@gmail.com>
2022-01-13 14:51:36 -0300

    Modificações no hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,18 +8,18 @@ function Header(){
 
     return(
 
-        <div className='header'>
+        <div className='hero'>
 
-            <div className='text-header'>
+            <div className='text-hero'>
                 <h1 className='titulo'>To Do</h1>
                 <h2 className='subtitulo'>Tenha total controle sobre suas tarefas</h2>
                 <button className='botao-iniciar'>Iniciar</button>
             </div>
 
             <div className='grid-img'>
-                <img className='header-img' alt='illustration-task' src={taskImg}></img>
-                <img className='header-img' alt='run' src={runImg}></img>
-                <img className='header-img' alt='study' src={studyImg}></img>
+                <img className='hero-img' alt='illustration-task' src={taskImg}></img>
+                <img className='hero-img' alt='run' src={runImg}></img>
+                <img className='hero-img' alt='study' src={studyImg}></img>
             </div>
 
         </div>

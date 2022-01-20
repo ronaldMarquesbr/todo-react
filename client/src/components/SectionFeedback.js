517d5a85c0a339f17a48cc2ecb5573f5eadf8272
Ronald Marques <ronaldpmarques01@gmail.com>
2022-01-19 21:58:23 -0300

    Estilização do background da seção de feedback

--- client/src/components/SectionFeedback.js
+++ client/src/components/SectionFeedback.js
@@ -6,21 +6,22 @@ function SectionFeedback(props){
 
     return(
 
-        <div className='section-feedback'>
+        <div className='container-feedback'>
+            <div className='section-feedback'>
 
-            <div className='card-feedback'>
-                <img src={personFeedbackImg} alt='person'></img>
-                <div className='text-feedback'>
-                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quasi vel vero veniam deleniti culpa, quis aliquid ab, quidem, suscipit officia rerum voluptatem libero maiores sed maxime assumenda a atque."
-                    <div className='footer-card-feedback'>
-                        <div>Lara Manuela</div>
-                        <div>Designer</div>
+                <div className='card-feedback'>
+                    <img src={personFeedbackImg} alt='person'></img>
+                    <div className='text-feedback'>
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quasi vel vero veniam deleniti culpa, quis aliquid ab, quidem, suscipit officia rerum voluptatem libero maiores sed maxime assumenda a atque."
+                        <div className='footer-card-feedback'>
+                            <div>Lara Manuela</div>
+                            <div>Designer</div>
+                        </div>
                     </div>
                 </div>
+            
             </div>
-           
         </div>
-
     )
 
 }

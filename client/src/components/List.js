44fe68a3ced52a34dcf7e82fb630efb118437ea0
Ronald Marques <ronaldpmarques01@gmail.com>
2022-02-08 09:10:58 -0300

    Animação descrição

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -7,7 +7,10 @@ function List(props){
         let inClassList = false;
 
         let desc = icon.parentNode.parentNode.querySelector('.desc-task');
+        let descParent = desc.parentNode;
+        
         let animClass = 'showDesc';
+        let marginDesc = 'margin-desc'
 
         for(let value of desc.classList){
 
@@ -22,10 +25,13 @@ function List(props){
         if(inClassList){
 
             desc.classList.remove(animClass);
+            descParent.classList.remove(marginDesc);
+
 
         } else {
 
             desc.classList.add(animClass);
+            descParent.classList.add(marginDesc);
 
         }
 
@@ -35,6 +41,46 @@ function List(props){
 
         <div className='list'>
             
+            <div>
+
+                <div id="list-item">
+                        
+                    <input type="checkbox"></input>
+                    <label>Teste</label>
+                    <i className='bx bxs-chevron-down' onClick={(event) => addAnimation(event.target)}></i>
+
+                </div>
+
+                <div className='desc-task'>
+
+                        <h4>Descrição:</h4>
+                        <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,</p>
+                        <button>Excluir</button>
+
+                </div>
+
+            </div>
+
+            <div>
+
+                <div id="list-item">
+                        
+                    <input type="checkbox"></input>
+                    <label>Teste</label>
+                    <i className='bx bxs-chevron-down' onClick={(event) => addAnimation(event.target)}></i>
+
+                </div>
+
+                <div className='desc-task'>
+
+                        <h4>Descrição:</h4>
+                        <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,</p>
+                        <button>Excluir</button>
+
+                </div>
+
+            </div>
+
             <div>
 
                 <div id="list-item">
